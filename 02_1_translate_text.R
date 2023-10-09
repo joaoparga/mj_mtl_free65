a0b1e38ccb728e4055f0ba77889b161b9737fbdb
Joao Parga <joaoparga@GEOG-FVFH73CAQ05D.local>
2023-10-08 22:03:03 -0400

    Code travel behaviour variables: count number of trips

--- 02_1_translate_text.R
+++ 02_1_translate_text.R
@@ -7,12 +7,16 @@ library(deeplr)
 
 # read data ---------------------------------------------------------------
 
-montreal_cd_ct <- sf::st_read("data/report/survey_filter_inside_cd.gpkg")
+survey_filter_cd <- sf::st_read("data/report/survey_filter_inside_cd.gpkg")
 
 # read key ----------------------------------------------------------------
 key <- data.table::fread("data/translate_key.csv", header = F) %>% 
   purrr::as_vector()
 
+
+# check key usage ---------------------------------------------------------
+
+deeplr::usage2(key)
 
 # translate ---------------------------------------------------------------
 
@@ -20,7 +24,7 @@ key <- data.table::fread("data/translate_key.csv", header = F) %>%
 # * q45_21 ----------------------------------------------------------------
 
 # DONE
-# dt_q45_21 <- montreal_cd_ct
+# dt_q45_21 <- survey_filter_cd
 # data.table::setDT(dt_q45_21)
 # dt_q45_21[
 #   !is.na(q45_21_text) & q45_21_text != "-99"
@@ -30,10 +34,33 @@ key <- data.table::fread("data/translate_key.csv", header = F) %>%
 #   ]
 # 
 # dt_q45_21[!is.na(q45_21_text) & q45_21_text != "-99", .(v1,q45_21_text, q45_21_trans)] %>% View()
-# 
+
+
+# ** save data ------------------------------------------------------------
+
+# dt_q45_21 <- dt_q45_21 %>% 
+#   select(v1, q45_21_text, q45_21_trans)
+# data.table::fwrite(dt_q45_21, "data/translated_q45_21.csv")
 
 # * q21_14_text --------------------------------------------------------------
+dt_q21_14_text <- survey_filter_cd
+data.table::setDT(dt_q21_14_text)
 
+dt_q21_14_text[
+  !is.na(q21_14_text) & q21_14_text != "-99"
+  , q21_14_text_trans := deeplr::translate2(
+    text = q21_14_text, target_lang = "EN", source_lang = "FR", auth_key = key
+  )
+  ]
+
+# dt_q21_14_text[!is.na(q21_14_text) & q21_14_text != "-99", .(v1,q45_21_text, q45_21_trans)] %>% View()
+
+
+# ** save data ------------------------------------------------------------
+
+dt_q21_14_text <- dt_q21_14_text %>% 
+  select(v1, q21_14_text, q21_14_text_trans)
+data.table::fwrite(dt_q21_14_text, "data/translated_q21_14_text.csv")
 
 # * q25_29_text -----------------------------------------------------------
 
@@ -48,8 +75,4 @@ key <- data.table::fread("data/translate_key.csv", header = F) %>%
 
 
 
-# save data ---------------------------------------------------------------
 
-dt_q45_21 <- dt_q45_21 %>% 
-  select(v1, q45_21_text, q45_21_trans)
-data.table::fwrite(dt_q45_21, "data/translated_q45_21.csv")
