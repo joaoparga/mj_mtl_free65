48b17c4c499c9fa2360450d3925301669c0d58e1
Joao Parga <joaoparga@GEOG-FVFH73CAQ05D.local>
2024-07-30 22:33:50 -0400

    Fix id variable for new dataset (response_id); fix old translated datasets; continue updating report tables

--- 05_1_wave2_translate_text.R
+++ 05_1_wave2_translate_text.R
@@ -19,6 +19,12 @@ df_wave2 <- data.table::fread("data/wave2_unified_data/JoinedData_17_11_2023_raw
 df_wave2 <-  janitor::clean_names(df_wave2)
 
 
+# remove duplicate --------------------------------------------------------
+
+# check 05_2_wave2_create_vars.qmd for explanation on why remove this row
+df_wave2 <- df_wave2 %>% 
+  filter(!v1==1424)
+
 # read key ----------------------------------------------------------------
 key <- data.table::fread("data/translate_key.csv", header = F) %>% 
   purrr::as_vector()
@@ -30,6 +36,50 @@ deeplr::usage2(key)
 
 # translate ---------------------------------------------------------------
 
+
+# OLD VARS ----------------------------------------------------------------
+survey_filter_cd <- sf::st_read("data/report/survey_filter_inside_cd.gpkg")
+# 
+# dt_q45_21 <- survey_filter_cd
+# data.table::setDT(dt_q45_21)
+# dt_q45_21[
+#   !is.na(q45_21_text) & q45_21_text != "-99"
+#   , q45_21_trans := deeplr::translate2(
+#     text = q45_21_text, target_lang = "EN", source_lang = "FR", auth_key = key
+#   )
+  # ]
+
+# dt_q45_21[!is.na(q45_21_text) & q45_21_text != "-99", .(response_id,q45_21_text, q45_21_trans)] %>% View()
+
+
+# dt_q45_21 <- dt_q45_21 %>%
+#   select(response_id, q45_21_text, q45_21_trans)
+# data.table::fwrite(dt_q45_21, "data/translated_q45_21_newidvar.csv")
+
+
+# NEW VARS ----------------------------------------------------------------
+
+dt_q21_14_text <- survey_filter_cd
+data.table::setDT(dt_q21_14_text)
+
+dt_q21_14_text[
+  !is.na(q21_14_text) & q21_14_text != "-99"
+  , q21_14_trans := deeplr::translate2(
+    text = q21_14_text, target_lang = "EN", source_lang = "FR", auth_key = key
+  )
+]
+
+dt_q21_14_text[
+  !is.na(q21_14_text) & q21_14_text != "-99", 
+  .(response_id, q21_14_text, q21_14_trans)] %>% View()
+
+
+
+# ** save data ------------------------------------------------------------
+
+dt_q21_14_text <- dt_q21_14_text %>% 
+  select(response_id, q21_14_text, q21_14_trans)
+data.table::fwrite(dt_q21_14_text, "data/translated_q21_14_text_newidvar.csv")
 
 # * q45_21 ----------------------------------------------------------------
 
@@ -43,14 +93,14 @@ deeplr::usage2(key)
 #   )
 #   ]
 
-# dt_v2_q45_21[!is.na(v2_q45_21_text) & v2_q45_21_text != "-99", 
-#              .(v1,v2_q45_21_text, v2_q45_21_trans)] %>% View()
+# dt_v2_q45_21[!is.na(v2_q45_21_text) & v2_q45_21_text != "-99",
+#              .(response_id,v2_q45_21_text, v2_q45_21_trans)] %>% View()
 
 
 # ** save data ------------------------------------------------------------
 
 # dt_v2_q45_21 <- dt_v2_q45_21 %>%
-#   select(v1, v2_q45_21_text, v2_q45_21_trans)
+#   select(response_id, v2_q45_21_text, v2_q45_21_trans)
 # data.table::fwrite(dt_v2_q45_21, "data/translated_v2_q45_21.csv")
 
 # * q21_14_text --------------------------------------------------------------
@@ -67,8 +117,8 @@ deeplr::usage2(key)
 #   )
 #   ]
 
-# dt_v2_q21_14_text[!is.na(v2_q21_14_text) & v2_q21_14_text != "-99", 
-#                   .(v1, v2_q21_14_text, v2_q21_14_text_trans)] %>% View()
+# dt_v2_q21_14_text[!is.na(v2_q21_14_text) & v2_q21_14_text != "-99",
+#                   .(response_id, v2_q21_14_text, v2_q21_14_trans)] %>% View()
 
 
 
@@ -79,8 +129,8 @@ deeplr::usage2(key)
 
 # ** save data ------------------------------------------------------------
 
-# dt_v2_q21_14_text <- dt_v2_q21_14_text %>% 
-#   select(v1, v2_q21_14_text, v2_q21_14_trans)
+# dt_v2_q21_14_text <- dt_v2_q21_14_text %>%
+#   select(response_id, v2_q21_14_text, v2_q21_14_trans)
 # data.table::fwrite(dt_v2_q21_14_text, "data/translated_v2_q21_14_text.csv")
 
 # * q25_29_text -----------------------------------------------------------
